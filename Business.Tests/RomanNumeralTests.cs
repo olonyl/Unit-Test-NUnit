79eb42b35cb832b6773e40f5453df53be39f3b83
olonyl <olony.rocha@contollo.net>
2021-10-11 10:53:43 -0500

    Roman Numeral Excersise

---
+++ Business.Tests/RomanNumeralTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Business.Tests
+{
+    public class RomanNumeralTests
+    {
+        [TestCaseSource(typeof(RomanNumeralSource))]
+        public void Parse(int expected, string roman)
+        {
+            Assert.AreEqual(expected, RomanNumeral.Parse(roman));
+        }
+        public class RomanNumeralSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new dynamic[] { 1, "I" };
+                yield return new dynamic[] { 5, "V" };
+                yield return new dynamic[] { 2, "II" };
+                yield return new dynamic[] { 4, "IV" };
+            }
+        }
+    }
+    
+}
---
+++ FirstUnitTest/RomanNumeral.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class RomanNumeral
+    {
+        private static readonly Dictionary<char, int> Map = new Dictionary<char, int>()
+        {
+            {'I', 1 },
+            {'V', 5 },
+            {'X', 10 },
+            {'L', 50  },
+            {'C', 100 },
+            {'D', 500 },
+            {'M', 1000 },
+        };
+
+        public static int Parse (string roman)
+        {
+            int result = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if(i+1 < roman.Length && IsSubtractive(roman[i], roman[i + 1]))
+                {
+                    result -= Map[roman[i]];
+                }
+                else
+                {
+                    result += Map[roman[i]];
+                }
+                
+            }
+            return result;
+        }
+
+        private static bool IsSubtractive(char v1, char v2)
+        {
+            return Map[v1] < Map[v2];
+        }
+    }
+}
