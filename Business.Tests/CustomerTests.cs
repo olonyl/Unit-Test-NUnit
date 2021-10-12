10e32d83268ba339ea1c1812e36d297e6d888229
olonyl <olony.rocha@contollo.net>
2021-10-12 09:55:15 -0500

    Trying to implement Unit Tests for the Customer class, the  code is not applicable for Unit Test, it must be refactored

---
+++ Business.Tests/CustomerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Tests
+{
+    public class CustomerTests
+    {
+        //[Test]
+        //public void CalculateWage_Id_
+    }
+}
---
+++ FirstUnitTest/TestDouble/Untestable/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.TestDouble.Untestable
+{
+    public class Customer
+    {
+        private readonly Logger _logger = new Logger();
+        public decimal CalculateWage(int id)
+        {
+            DbGateway gateway = new DbGateway();
+            WorkingStatistics ws = gateway.GetWorkingStatistics(id);
+
+            decimal wage;
+            if (ws.PayHourly)
+            {
+                wage = ws.WorkingHours * ws.HourSalary;
+            }
+            else
+            {
+                wage = ws.MonthSalary;
+            }
+            _logger.Info($"Customer ID={id}, Wage:{wage}");
+
+            return wage;
+        }
+    }   
+}
+
+internal class Logger
+{
+    public void Info(string s)
+    {
+        File.WriteAllText(@"C:\tmp:\log.txt", s);
+    }
+}
+
+public class DbGateway
+{
+    public WorkingStatistics GetWorkingStatistics(int id)
+    {
+        //a real gateway can experience any possible problems
+        //like "no connection" throwing an exception
+        throw new NoConnection();
+    }
+}
+
+public class NoConnection : Exception
+{
+}
+
+public class WorkingStatistics
+{
+    public decimal HourSalary { get; set; }
+    public int WorkingHours { get; set; }
+    public decimal MonthSalary { get; set; }
+    public bool PayHourly { get; set; }
+}
