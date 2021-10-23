de6a02867cf8d4969495bb1f63ee6a106fa60784
olonyl <olony.rocha@contollo.net>
2021-10-22 19:36:33 -0500

    TDD - Use Task.Factory and Stopwatch to validate Execution Time

---
+++ FirstUnitTest/Client.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Client
+    {
+        private readonly UpdateableSpin spin;
+        private readonly Processor processor;
+        private readonly double maxTimeoutToWait;
+        private readonly object timeout;
+
+        public Client()
+        {
+            processor = new Processor();
+            processor.ResponseReceived += response =>
+            {
+                spin.UpdateTimeOut();
+                Console.Write(response);
+            };
+            processor.ProcessingFinished += () => spin.Set();
+            spin = new UpdateableSpin(shouldWait: true);
+        }
+        public void Do()
+        {
+            processor.StartAsyncExchange(timeout);
+            if (!spin.Wait(TimeSpan.FromSeconds(maxTimeoutToWait)))
+            {
+                Console.WriteLine("Time out!");
+            }
+            else
+            {
+                Console.WriteLine("Finished in time.");
+            }
+        }
+    }
+
+    internal class Processor
+    {
+        internal Action<object> ResponseReceived;
+        internal Func<object> ProcessingFinished;
+
+        internal void StartAsyncExchange(object timeout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class UpdateableSpin
+    {
+        private bool shouldWait;
+
+        public UpdateableSpin(bool shouldWait)
+        {
+            this.shouldWait = shouldWait;
+        }
+
+        internal object Set()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UpdateTimeOut()
+        {
+            //throw new NotImplementedException();
+        }
+
+        internal bool Wait(TimeSpan timeSpan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDD-UnitTest/UpdateableSpinTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDD_UnitTest
+{
+    public class UpdateableSpinTests
+    {
+        [Test]
+        public void Wait_NoPulse_ReturnsFalse()
+        {
+            UpdateableSpin spin = new UpdateableSpin();
+            bool wasPulsed = spin.Wait(TimeSpan.FromMilliseconds(10));
+            Assert.IsFalse(wasPulsed);
+        }
+
+        [Test]
+        public void Wait_Pulse_ReturnsTrue()
+        {
+            UpdateableSpin spin = new UpdateableSpin();
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(100);
+                spin.Set();
+            });
+            bool wasPulsed = spin.Wait(TimeSpan.FromSeconds(10));
+            Assert.IsTrue(wasPulsed);
+        }
+
+        [Test]
+        public void Wait_50Millisec_ActuallyWaitingFor50Millisec()
+        {
+            var spin = new UpdateableSpin();
+            
+            Stopwatch watcher = new Stopwatch();
+            watcher.Start();
+
+            spin.Wait(TimeSpan.FromMilliseconds(50));
+
+            watcher.Stop();
+
+            TimeSpan actual = TimeSpan.FromMilliseconds(watcher.ElapsedMilliseconds);
+            TimeSpan leftEpsilon = TimeSpan.FromMilliseconds(50 - (100 * 0.1));
+            TimeSpan rightEpsilon = TimeSpan.FromMilliseconds(50 + (100 * 0.1));
+
+            Assert.IsTrue(actual > leftEpsilon && actual < rightEpsilon);
+
+        }
+        [Test]
+        public void Wait500Mililisec_UpdateAfter300Millisec_totalWaitingIsApprox800Millisec()
+        {
+            var spin = new UpdateableSpin();
+
+            Stopwatch watcher = new Stopwatch();
+            watcher.Start();
+
+            const int timeout = 500;
+            const int spanBeforeUpdate = 300;
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(timeout);
+                spin.UpdateTimeout();
+            });
+
+            spin.Wait(TimeSpan.FromMilliseconds(timeout));
+            watcher.Stop();
+
+            TimeSpan actual = TimeSpan.FromMilliseconds(watcher.ElapsedMilliseconds);
+            const int expected = timeout + spanBeforeUpdate;
+            
+            TimeSpan left = TimeSpan.FromMilliseconds(expected - (expected+100 * 0.1));
+            TimeSpan right = TimeSpan.FromMilliseconds(expected + (expected+100 * 0.1));
+
+            Assert.IsTrue(actual > left && actual < right);
+        }
+    }
+
+  
+
+    internal class UpdateableSpin
+    {
+        private readonly Object lockObj = new Object();
+        private bool shouldWait = true;
+        private long executionStartingTime;
+
+        internal bool Wait(TimeSpan timeout, int spintDuration = 0)
+        {
+            UpdateTimeout();
+            while (true)
+            {
+                lock (lockObj)
+                {
+                    if (!shouldWait)
+                        return true;
+                    if (DateTime.UtcNow.Ticks - executionStartingTime > timeout.Ticks)
+                        return false;
+                }
+                Thread.Sleep(spintDuration);
+            }
+        }
+
+        internal void Set()
+        {
+            lock (lockObj)
+            {
+                shouldWait = false;
+            }
+        }
+
+        internal void UpdateTimeout()
+        {
+            lock (lockObj)
+            {
+                executionStartingTime = DateTime.UtcNow.Ticks;
+            }
+        }              
+    }
+}
